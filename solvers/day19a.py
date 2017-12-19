9e097e3c440be4d34942af22dcd7bacc360821f4
Grasseh <steve.gagnev4si@gmail.com>
2017-12-19 09:48:59 -0500

    Day 19: A Series of Tubes

---
+++ solvers/day19a.py
@@ -0,0 +1,52 @@
+# pylint: disable=missing-docstring
+import unittest
+
+def solve(fn_input):
+    instructions = fn_input[:-1].split("\n")
+    #width = len(instructions[0])
+    #height = len(instructions)
+    y = 0
+    x = instructions[0].index("|")
+    done = False
+    direction = "S"
+    collection = ""
+    while not done:
+        if direction == "S":
+            y += 1
+        if direction == "N":
+            y -= 1
+        if direction == "E":
+            x += 1
+        if direction == "W":
+            x -= 1
+        if instructions[y][x] == "|" or instructions[y][x] == "-":
+            continue
+        if instructions[y][x] == "+":
+            direction = change_direction(direction, x, y, instructions)
+            continue
+        if instructions[y][x] != " ":
+            collection += instructions[y][x]
+            continue
+        return collection
+
+def change_direction(direction, x, y, instructions):
+    if direction != "S" and instructions[y-1][x] != " ":
+        return "N"
+    if direction != "N" and instructions[y+1][x] != " ":
+        return "S"
+    if direction != "W" and instructions[y][x+1] != " ":
+        return "E"
+    if direction != "E" and instructions[y][x-1] != " ":
+        return "W"
+
+
+class UnitTest(unittest.TestCase):
+    def test_solve(self):
+        input_str  = "     |          \n"
+        input_str += "     |  +--+    \n"
+        input_str += "     A  |  C    \n"
+        input_str += " F---|----E|--+ \n"
+        input_str += "     |  |  |  D \n"
+        input_str += "     +B-+  +--+ \n"
+        input_str += "                \n"
+        self.assertEqual(solve(input_str), "ABCDEF")
