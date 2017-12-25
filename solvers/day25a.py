dad4b2b986a078f29f20f096f69c8c0dc9262a63
Grasseh <steve.gagnev4si@gmail.com>
2017-12-25 16:50:55 -0500

    Day 25: The Halting Problem

---
+++ solvers/day25a.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-docstring
+import unittest
+import re
+
+def solve(fn_input):
+    return True
+
+class UnitTest(unittest.TestCase):
+    def test_solve(self):
+        self.assertEqual(solve("Thank you Eric for this freebie"), True)
